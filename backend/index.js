bd06eb1b15fd98c6025bf5630eb3c155114054bc
Amaan325 <thequreshisaha@gmail.com>
2024-05-05 22:53:58 +0500

    create connection with database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000 , () => {
-    console.log(`Server running on port 3000!`);
-})
+app.listen(3000, () => {
+  console.log(`Server running on port 3000!`);
+});
+
+mongoose.connect(process.env.mongodb_url).then(() => {
+  console.log("Connected with Database");
+}).catch((error) => {
+  console.error("Error connecting to database:", error);
+});
