f77c3935a1c150860e82ad288e272bf976fb177b
Amaan325 <thequreshisaha@gmail.com>
2024-05-09 20:53:35 +0500

    Added SignUp functionality

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const router = require("./src/routes/userRoutes");
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors({origin:"http://localhost:5173"  ,credentials: true}));
 app.listen(3000, () => {
   console.log(`Server running on port 3000!`);
 });
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,39 +1,83 @@
-import React from "react";
-import {NavLink} from "react-router-dom"
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await axios.post(
+        "http://localhost:3000/user/signup",
+        formData,
+        {
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      if (response.status !== 200) {
+        setError(true);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
-    <div>
+    <div className="max-w-lg mx-auto">
       <h1 className="font-semibold text-2xl text-center my-3">SignUp</h1>
-      <form className="flex flex-col gap-3 max-w-lg mx-auto">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 ">
         <input
           type="text"
           id="username"
           placeholder="Username"
           className="bg-slate-200 rounded-lg p-2 text-[15px]"
+          onChange={handleChange}
         ></input>
         <input
           type="email"
           id="email"
           placeholder="Email"
           className="bg-slate-200 rounded-lg p-2 text-[15px]"
+          onChange={handleChange}
         ></input>
         <input
           type="password"
           id="password"
           placeholder="Password"
           className="bg-slate-200 rounded-lg p-2 text-[15px]"
+          onChange={handleChange}
         ></input>
-        <button className="bg-slate-700 text-[15px] text-white uppercase p-2 rounded-lg hover:bg-slate-800">
-          Signup
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-[15px] text-white uppercase p-2 rounded-lg hover:bg-slate-800 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
-      <div className="flex gap-1 max-w-lg mx-auto my-3">
+      <div className="flex gap-2 items-center  my-3">
         <p className="text-[15px] max-2-">Have an account?</p>
         <NavLink to="/signin">
-        <span className="text-[15px] text-blue-500 cursor-pointer">Sign In</span>
+          <span className="text-[15px] text-blue-500 cursor-pointer">
+            Sign In
+          </span>
         </NavLink>
       </div>
+      <p className="text-red-500 text-[16px] ">
+        {error && "Something went wrong!!!"}
+      </p>
     </div>
   );
 };
